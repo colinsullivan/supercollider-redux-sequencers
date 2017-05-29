eb372a208b05d86818cba963995515965803834b
Colin Sullivan <colin@colin-sullivan.net>
2017-05-28 20:27:12 -0700

    expose playing states

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import * as actions from "./actions"
 import * as actionTypes from "./actionTypes"
-import reducer, { create_default_sequencer } from "./reducers"
+import reducer, { create_default_sequencer, PLAYING_STATES } from "./reducers"
 
 export default {
   actions,
   actionTypes,
   reducer,
-  create_default_sequencer
+  create_default_sequencer,
+  PLAYING_STATES
 }
