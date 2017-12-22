c3bc312b38e41a00e9cabd2f0db67e45457950e2
Colin Sullivan <colin@colin-sullivan.net>
2017-12-22 16:33:21 -0500

    updated parameter example boilerplate file

--- src/parameter_example.js
+++ src/parameter_example.js
@@ -14,10 +14,11 @@
 
 
 
-import { createStore } from "redux"
+import { createStore, combineReducers } from "redux"
+import sc from 'supercolliderjs';
 import supercolliderRedux from "supercollider-redux"
 import abletonLinkRedux from "abletonlink-redux"
-import SCStoreController from "./SCStoreController"
+const SCStoreController = supercolliderRedux.SCStoreController;
 const AbletonLinkController = abletonLinkRedux.AbletonLinkController;
 import awakeningSequencers from "."
 
@@ -31,69 +32,97 @@ function create_default_state () {
   };
 }
 
-var rootReducer = function (state = create_default_state(), action) {
+var rootReducer = combineReducers({
+  [abletonLinkRedux.DEFAULT_MOUNT_POINT]: abletonLinkRedux.reducer,
+  [supercolliderRedux.DEFAULT_MOUNT_POINT]: supercolliderRedux.reducer,
+  sequencers: function (state, action) {
 
-  //state.simpleSound = simpleSound(state.simpleSound, action);
-  state.abletonlink = abletonLinkRedux.reducer(state.abletonlink, action);
-  state.supercolliderRedux = supercolliderRedux.reducer(state.supercolliderRedux, action);
+    state = awakeningSequencers.reducer(state, action);
 
-  state.sequencers = awakeningSequencers.reducer(state.sequencers, action);
+    switch (action.type) {
+      case "PARAM_EXAMPLE_LEGATO":
+        state.paramexample.releaseTime = 1.2;
+        
+        break;
 
-  switch (action.type) {
-    case "PARAM_EXAMPLE_LEGATO":
-      state.sequencers.paramexample.releaseTime = 1.2;
+      case "PARAM_EXAMPLE_STOCCATO":
+        state.paramexample.releaseTime = 0.2;
+        break;
       
-      break;
+      default:
+        break;
+    }
 
-    case "PARAM_EXAMPLE_STOCCATO":
-      state.sequencers.paramexample.releaseTime = 0.2;
-      break;
+    return state;
     
-    default:
-      break;
-  }
-
-  return state;
-  
-};
-
-var store = createStore(rootReducer);
-var scStoreController = new SCStoreController(store);
-var abletonLinkController = new AbletonLinkController(store, 'abletonlink');
-
-let paramexampleReady = false;
-store.subscribe(() => {
-  let state = store.getState();
-  let newIsReady = state.sequencers.paramexample.isReady;
-
-  if (newIsReady != paramexampleReady) {
-    console.log("Queueing metronome...");
-    paramexampleReady = newIsReady;
-    store.dispatch(awakeningSequencers.actions.sequencerQueued('paramexample'));
   }
 });
 
-setInterval(() => {
-  console.log("store.getState()");
-  console.log(store.getState());
-}, 1000);
+var store = createStore(rootReducer, create_default_state());
+sc.lang.boot().then((sclang) => {
+  sclang.interpret('API.mountDuplexOSC();').then(() => {
+    var scStoreController = new SCStoreController(store);
+    var abletonLinkController = new AbletonLinkController(store, 'abletonlink');
 
-setTimeout(() => {
-  store.dispatch({
-    type: "PARAM_EXAMPLE_LEGATO"
-  });
+    let paramexampleReady = false;
+    store.subscribe(() => {
+      let state = store.getState();
+      let newIsReady = state.sequencers.paramexample.isReady;
 
-  setTimeout(() => {
-    store.dispatch({
-      type: "PARAM_EXAMPLE_STOCCATO"
+      if (newIsReady != paramexampleReady) {
+        console.log("Queueing metronome...");
+        paramexampleReady = newIsReady;
+        store.dispatch(awakeningSequencers.actions.sequencerQueued('paramexample'));
+      }
     });
 
+    sclang.interpret(`
+  var store, sequencers;
+
+  s.waitForBoot({
+    store = StateStore.getInstance();
+    sequencers = IdentityDictionary.new();
+
+    // when state changes, this method will be called
+    store.subscribe({
+      var state = store.getState();
+
+
+      if ((state.sequencers != nil) && (state.sequencers.paramexample != nil) && (sequencers['paramexample'] == nil), {
+        sequencers['paramexample'] = ParamExampleSequencer.new((store: store, sequencerId: 'paramexample'));
+      });
+
+
+    });
+  });
+
+  s.boot();
+
+    `);
+
+    setInterval(() => {
+      console.log("store.getState()");
+      console.log(store.getState());
+    }, 1000);
+
     setTimeout(() => {
-      store.dispatch(awakeningSequencers.actions.sequencerStopQueued('paramexample'));
+      store.dispatch({
+        type: "PARAM_EXAMPLE_LEGATO"
+      });
+
+      setTimeout(() => {
+        store.dispatch({
+          type: "PARAM_EXAMPLE_STOCCATO"
+        });
+
+        setTimeout(() => {
+          store.dispatch(awakeningSequencers.actions.sequencerStopQueued('paramexample'));
+        }, 6000);
+
+      }, 6000);
+
     }, 6000);
 
-  }, 6000);
 
-}, 6000);
-
-
+  });
+});
