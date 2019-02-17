98135564e4c716ee8e2103db1ea68df40dc84e08
Colin Sullivan <colin@colin-sullivan.net>
2019-02-16 23:29:14 -0800

    travis

--- .travis/install_quark.sh
+++ .travis/install_quark.sh
@@ -2,7 +2,5 @@ echo "Quarks.install(\"$TRAVIS_BUILD_DIR/quarks/awakening-sequencers\");" >> $TR
 echo "thisProcess.shutdown();" >> $TRAVIS_BUILD_DIR/install_quark.sc
 echo "0.exit();" >> $TRAVIS_BUILD_DIR/install_quark.sc
 
-echo "which nvm: $(which nvm)"
-
 sclang $TRAVIS_BUILD_DIR/install_quark.sc
 exit 0
