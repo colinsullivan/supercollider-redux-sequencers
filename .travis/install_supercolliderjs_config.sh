0f70d0f53aafade3e3d7c31ce57e08223a0dc048
Colin Sullivan <colin@colin-sullivan.net>
2019-02-16 22:46:24 -0800

    travis

--- .travis/install_supercolliderjs_config.sh
+++ .travis/install_supercolliderjs_config.sh
@@ -2,8 +2,3 @@
 
 cp $TRAVIS_BUILD_DIR/.travis/.supercollider.yaml $TRAVIS_BUILD_DIR/.supercollider.yaml
 echo "sclang_conf: $TRAVIS_BUILD_DIR/.travis/sclang_conf.yaml" >> $TRAVIS_BUILD_DIR/.supercollider.yaml
-
-echo ".supercollider.yaml:"
-cat $TRAVIS_BUILD_DIR/.supercollider.yaml
-
-
