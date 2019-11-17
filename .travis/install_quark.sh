c5576e60472413c74051a7efd28c17f64b7cc25d
Colin Sullivan <colin@colin-sullivan.net>
2019-11-17 15:39:33 +0000

    explicitly call install on supercollider-redux quark because deps are not installed

--- .travis/install_quark.sh
+++ .travis/install_quark.sh
@@ -1,4 +1,5 @@
 echo "Quarks.install(\"$TRAVIS_BUILD_DIR/quarks/supercollider-redux-sequencers\");" >> $TRAVIS_BUILD_DIR/install_quark.sc
+echo "Quarks.install(\"/home/travis/.local/share/SuperCollider/downloaded-quarks/supercollider-redux/quarks/supercollider-redux\");" >> $TRAVIS_BUILD_DIR/install_quark.sc
 echo "thisProcess.shutdown();" >> $TRAVIS_BUILD_DIR/install_quark.sc
 echo "0.exit();" >> $TRAVIS_BUILD_DIR/install_quark.sc
 sclang $TRAVIS_BUILD_DIR/install_quark.sc
