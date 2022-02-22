bf5458beff6db52e2757d882c7aa0396dea6b20d
Colin Sullivan <colin@colin-sullivan.net>
2022-02-21 18:39:11 -0800

    v0.9.11

--- lib/selectors.d.ts
+++ lib/selectors.d.ts
@@ -1,5 +1,9 @@
 /**
  *  Simplify sequencers for sending to SC.
  **/
-export const getSCSequencers: any;
-export const getSCState: any;
+export const getSCSequencers: import("reselect").OutputSelector<any, {}, (res: any) => {}>;
+export const getSCState: import("reselect").OutputSelector<any, {
+    sequencers: {};
+}, (res: {}) => {
+    sequencers: {};
+}>;
